5b5611446a331396ba1484eb608e86250165dd34
Ajay <venkataajay18@gmail.com>
2020-06-11 09:47:18 +0530

    Add logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,17 @@ class SessionsController < ApplicationController
   	user=User.find_by(email: user_email)
   	if user && user.authenticate(user_password)
 
+  		log_in user
   		redirect_to user_url(user)
 
   	else
-  		
+
   		flash[:danger] = "Email or password is incorrect."
   		render 'new'
     end
+  end
+  def destroy
+  	log_out
+  	redirect_to root_url
   end
 end
