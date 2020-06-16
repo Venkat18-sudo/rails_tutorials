e37a3216930055e6e3253980140a99b003254af6
Ajay <venkataajay18@gmail.com>
2020-06-16 08:49:27 +0530

    Add Update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
   	@user=User.new
 
@@ -17,6 +20,21 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success]="Profile Successfully updated !"
+        redirect_to user_url(@user)
+      else
+        flash[:danger]="Something went Wrong!"
+        render 'edit'
+
+      end
+
+  end
 
   private
 
@@ -24,4 +42,16 @@ class UsersController < ApplicationController
 
   			params.require(:user).permit(:name, :email, :password, :password_confirmation)
   		end  	
+
+      def logged_in_user
+        if logged_in?
+        else
+          flash[:danger] = "please log in."
+          redirect_to login_url
+        end
+      end
+      def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless  @user == current_user
+      end
 end
